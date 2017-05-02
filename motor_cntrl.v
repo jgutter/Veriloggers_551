7ec82e54fa9cec2919f424e7d093e15040e5a660
sbertz <sbertz@wisc.edu>
2017-05-02 11:01:57 -0500

    Add files via upload

--- motor_cntrl.v
+++ motor_cntrl.v
@@ -1,9 +1,9 @@
 module motor_cntrl(clk, rst_n, lft, rht, fwd_lft, rev_lft, fwd_rht, rev_rht);
 
-input [10:0] lft, rht;  //input signals
+input [10:0] lft, rht;
 
-output reg fwd_lft, fwd_rht, rev_lft, rev_rht;  //latched outputs.
-input clk, rst_n;  //Global clock and reset.
+output reg fwd_lft, fwd_rht, rev_lft, rev_rht;
+input clk, rst_n;
 
 
 reg sign_l, sign_r; //Sign of the incomming lft and rht sig. 0 is negative, 1 is positive
@@ -12,7 +12,6 @@ reg pre_fl, pre_rl, pre_fr, pre_rr;  //Input logic to fwd and rev registers.
 reg [10:0] two_comp_lft, two_comp_rht; //Keeps track of twos compliment of lft and rht.
 wire pwm_l, pwm_r;  //PWM signals comming from left and right signals.
 //DUT for pwm generator
-//Instantiates 2 DUTs , one for left and one for right signals.
 // module p_w_m(PWM_sig, duty, clk, rst_n);
 p_w_m pwml(pwm_l, mag_l, clk, rst_n);
 p_w_m pwmr(pwm_r, mag_r, clk, rst_n);
@@ -22,15 +21,16 @@ p_w_m pwmr(pwm_r, mag_r, clk, rst_n);
 
 
 //Input logic to fwd_*, rev_*
+
 always@(*) begin
 
-assign pre_fl = (sign_l == 1'b1) ? pwm_l: //Check sign then assign pwm if its positive or zero otherwise.
+assign pre_fl = (sign_l == 1'b1) ? pwm_l:
                  1'b0;
 
 assign pre_fr = (sign_r == 1'b1) ? pwm_r:
                  1'b0;
 
-assign pre_rl = (sign_l == 1'b0) ? pwm_l://Check sign then assign pwm if negative or zero elsewise.
+assign pre_rl = (sign_l == 1'b0) ? pwm_l:
                  1'b0;
 
 assign pre_rr = (sign_r == 1'b0) ? pwm_r:
@@ -51,7 +51,7 @@ always@(posedge clk, negedge rst_n)begin
 
   end
   else begin
-     fwd_lft <= pre_fl;  //Take in pre set values on each rising clock edge.
+     fwd_lft <= pre_fl;
      fwd_rht <= pre_fr;
      rev_lft <= pre_rl;
      rev_rht <= pre_rr;
@@ -70,10 +70,10 @@ end
 always@(posedge clk, negedge rst_n) begin
   if(!rst_n)
      mag_l <= 10'b0000000000;
-  else if ( lft[10] == 0)  //Check if the number is positive.
+  else if ( lft[10] == 0)
      mag_l <= lft[9:0];
   else 
-     mag_l <= two_comp_lft[9:0];  //If negative assign 2's comp value
+     mag_l <= two_comp_lft[9:0];
 end
 
 //Keeps track of the magnitude of rht.
@@ -95,7 +95,7 @@ always@(posedge clk, negedge rst_n) begin
 
   if(!rst_n)
      sign_l <= 1'b0;
-  else if (lft[10]==0)  //Assign 1 for positive value, 0 for negative
+  else if (lft[10]==0)
      sign_l <=1;
   else
      sign_l <=0;
@@ -108,7 +108,7 @@ always@(posedge clk, negedge rst_n) begin
 
   if(!rst_n)
      sign_r <= 1'b0;
-  else if (rht[10]==0) //Assign 1 for positive value, 0 for negative
+  else if (rht[10]==0)
      sign_r <=1;
   else
      sign_r <=0;
